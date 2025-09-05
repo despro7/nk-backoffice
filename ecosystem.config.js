b1b3c6142202d1918715c982f2531eafdb890e3c
despro7 <g.dziov@gmail.com>
2025-09-05 07:31:41 +0300

    Refactor ecosystem.config.js to use ES module syntax and add new npm scripts for cleaning and rebuilding the project.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,24 @@
-module.exports = {
-	apps: [{
-	  name: 'nk-backoffice',
-	  script: 'dist/server/index.js',
-	  instances: 1,
-	  autorestart: true,
-	  watch: false,
-	  max_memory_restart: '1G',
-	  env: {
-		NODE_ENV: 'production',
-		PORT: 3001
-	  },
-	  env_production: {
-		NODE_ENV: 'production',
-		PORT: 3001
-	  },
-	  error_log: './logs/app-error.log',
-	  out_log: './logs/app-out.log',
-	  log_log: './logs/app-combined.log',
-	  time: true,
-	  merge_logs: true,
-	  log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
-	}]
-  };
+export default {
+  apps: [{
+    name: 'nk-backoffice',
+    script: 'dist/server/index.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3001
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3001
+    },
+    error_log: './logs/app-error.log',
+    out_log: './logs/app-out.log',
+    log_log: './logs/app-combined.log',
+    time: true,
+    merge_logs: true,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+  }]
+};
