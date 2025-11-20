c62742c5bc5bf8663f40ce2f46daaaa985b3a376
despro7 <g.dziov@gmail.com>
2025-11-20 17:08:45 +0200

    feat: add PostCSS plugin for oklab() color function support

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 export default {
   plugins: {
+    // Конвертує OKLCH кольори в RGB з фолбеком для старіших браузерів
+    "@csstools/postcss-oklab-function": {
+      preserve: true, // Зберігає оригінальний OKLCH + додає RGB фолбек
+    },
     "@tailwindcss/postcss": {},
   },
 };
